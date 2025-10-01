bc2ecc89b6eeb877e54c96c16419de389fff4ddf
EnzoParada <paradaenzo26@outlook.com>
2025-10-01 18:50:48 -0300

    avances

--- src/com/techlab/app/Main.java
+++ src/com/techlab/app/Main.java
@@ -1,4 +1,36 @@
 package com.techlab.app;
 
+import com.techlab.productos.Producto;
+import com.techlab.productos.ServiciosProductos;
+
 public class Main {
+    public static void main(String[] args) {
+        ServiciosProductos sistema = new ServiciosProductos();
+
+        System.out.println("""
+                =================================================
+                         Bienvenido a la app de compras
+                =================================================      
+                """);
+
+        System.out.println("""
+                OPCIONES:
+                1-Agregar producto
+                2-Listar productos
+                3-Buscar/Actualizar producto
+                4-Eliminar producto
+                5-Crear pedido
+                6-Listar pedidos
+                7-Salir
+                """);
+        try{
+            sistema.agregarProducto("Bolsa de Granos de Café Premium (500g)", 12.75, 90);
+            sistema.agregarProducto("Aceite de Oliva Extra Virgen (1 Litro)",8.50,210);
+            sistema.agregarProducto("Caja de 12 Yogurt Natural",5.99,155);
+            sistema.traerLista();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
---
+++ src/com/techlab/productos/ServiciosProductos.java
@@ -0,0 +1,32 @@
+package com.techlab.productos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiciosProductos {
+    private List<Producto> lstProductos;
+
+    public ServiciosProductos() {
+        this.lstProductos = new ArrayList<Producto>();
+    }
+
+    public List<Producto> getLstProductos() {
+        return lstProductos;
+    }
+
+    public void agregarProducto(String nombre,double precio, int stock)throws Exception {
+        for(Producto p: lstProductos){
+            if(p.getNombre().equals(nombre)){
+                throw new Exception("Ya existe este producto: "+p.getNombre() );
+            }
+        }
+        Producto producto = new Producto(nombre,precio,stock);
+        lstProductos.add(producto);
+    }
+    public void traerLista(){
+       for (Producto p:lstProductos){
+            System.out.println(p);
+       }
+    }
+
+}
